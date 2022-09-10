93ad3fb7762a1cd84d668dd6f15fd402eca10945
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2022-09-11 04:59:57 +0700

    Updated configuration.

--- build/BuildPackages.cmd
+++ build/BuildPackages.cmd
@@ -1,19 +1,19 @@
 @ECHO OFF
 
 
-:INITIALIZE_PARAMETERS
+:INITIALIZE_BUILD_PARAMETERS
 SET %1
 SET %2
 
-SET CONFIGURATION=Release
+SET BUILD_CONFIGURATION=Release
 SET BUILD_VERSION=1.0.0
 
-GOTO SET_CONFIGURATION
+GOTO SET_BUILD_CONFIGURATION
 
 
-:SET_CONFIGURATION
-IF "%config%"=="" GOTO SET_BUILD_VERSION
-SET CONFIGURATION=%config%
+:SET_BUILD_CONFIGURATION
+IF "%configuration%"=="" GOTO SET_BUILD_VERSION
+SET BUILD_CONFIGURATION=%configuration%
 
 GOTO SET_BUILD_VERSION
 
@@ -28,12 +28,12 @@ GOTO BUILD
 :BUILD
 
 ECHO ----------------------------------------------------
-ECHO Building "%CONFIGURATION%" packages with version "%BUILD_VERSION%"...
+ECHO Building "%BUILD_CONFIGURATION%" packages with version "%BUILD_VERSION%"...
 ECHO ----------------------------------------------------
 
-dotnet build "NHibernate.ObservableCollections.sln" --configuration %CONFIGURATION% --property:PACKAGE_VERSION=%BUILD_VERSION% || EXIT /B 1
+dotnet build "NHibernate.ObservableCollections.sln" --configuration %BUILD_CONFIGURATION% --property:PACKAGE_VERSION=%BUILD_VERSION% || EXIT /B 1
 
-dotnet build "tools\Explicit.NuGet.Versions\Explicit.NuGet.Versions.sln" --configuration "Release"
+dotnet build "tools\Explicit.NuGet.Versions\Explicit.NuGet.Versions.sln" --configuration Release
 "tools\Explicit.NuGet.Versions\bin\nev.exe" "bin" "NHibernate.ObservableCollections."
 
 GOTO TEST
@@ -41,6 +41,8 @@ GOTO TEST
 
 :TEST
 
+REM https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-test
+REM https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-vstest
 REM https://github.com/Microsoft/vstest-docs/blob/main/docs/report.md
 REM https://github.com/spekt/nunit.testlogger/issues/56
 
@@ -48,12 +50,10 @@ ECHO ------------------------------------
 ECHO Running .NET (net6.0) Unit Tests
 ECHO ------------------------------------
 
-dotnet test "src\NHibernate.ObservableCollections.Tests" --configuration %CONFIGURATION% --framework net6.0 --no-build --output "bin\%CONFIGURATION%\net6.0" --results-directory "bin\%CONFIGURATION%" --logger "nunit;LogFileName=NHibernate.ObservableCollections-Net-TestResults.xml"
-REM dotnet "bin\%CONFIGURATION%\net6.0\NHibernate.ObservableCollections.Tests.dll" --result="bin\%CONFIGURATION%\NHibernate.ObservableCollections-Net-TestResults.xml;format=nunit3"
+dotnet test "bin\%BUILD_CONFIGURATION%\net6.0\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.dll" --results-directory "bin\%BUILD_CONFIGURATION%" --logger "nunit;LogFileName=NHibernate.ObservableCollections.Tests-Net-TestResults.xml;format=nunit3"
 
 ECHO --------------------------------------------
 ECHO Running .NET Framework (net48) Unit Tests
 ECHO --------------------------------------------
 
-dotnet test "src\NHibernate.ObservableCollections.Tests" --configuration %CONFIGURATION% --framework net48 --no-build --output bin\%CONFIGURATION%\net48 --results-directory bin\%CONFIGURATION% --logger "nunit;LogFileName=NHibernate.ObservableCollections-NetFramework-TestResults.xml;format=nunit3"
-REM "%UserProfile%\.nuget\packages\nunit.consolerunner\3.15.0\tools\nunit3-console.exe" "bin\%CONFIGURATION%\net48\NHibernate.ObservableCollections.Tests.exe" --result="bin\%CONFIGURATION%\NHibernate.ObservableCollections-NetFramework-TestResults.xml;format=nunit3"
+dotnet test "bin\%BUILD_CONFIGURATION%\net48\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.exe" --results-directory "bin\%BUILD_CONFIGURATION%" --logger "nunit;LogFileName=NHibernate.ObservableCollections.Tests-NetFramework-TestResults.xml;format=nunit3"
