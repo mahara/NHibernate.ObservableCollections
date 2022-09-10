3cb7e4231235f801e2e7ccfdd94f70415c69d8c0
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2022-09-11 04:59:57 +0700

    Updated configuration.

--- CleanPackages.cmd
+++ CleanPackages.cmd
@@ -1,11 +1,32 @@
 @ECHO OFF
 
 
-SET OUTPUT_FOLDER=bin
-SET TEMPORARY_OUTPUT_FOLDER=obj
+SET OUTPUT_FOLDER_PATH=bin
+SET TEMPORARY_OUTPUT_FOLDER_PATH=obj
+SET NEV_BIN_FOLDER_PATH=tools\Explicit.NuGet.Versions\bin
+SET NEV_OBJ_FOLDER_PATH=tools\Explicit.NuGet.Versions\obj
 
-dotnet clean "%1" --configuration Debug
-dotnet clean "%1" --configuration Release
+REM dotnet clean "%1" --configuration Debug
+REM dotnet clean "%1" --configuration Release
 
-IF EXIST "%OUTPUT_FOLDER%" RMDIR "%OUTPUT_FOLDER%" /S /Q
-IF EXIST "%TEMPORARY_OUTPUT_FOLDER%" RMDIR "%TEMPORARY_OUTPUT_FOLDER%" /S /Q
+IF EXIST "%OUTPUT_FOLDER_PATH%" (
+    ECHO Deleting "%OUTPUT_FOLDER_PATH%" folder...
+
+    RMDIR "%OUTPUT_FOLDER_PATH%" /S /Q
+)
+IF EXIST "%TEMPORARY_OUTPUT_FOLDER_PATH%" (
+    ECHO Deleting "%TEMPORARY_OUTPUT_FOLDER_PATH%" folder...
+
+    RMDIR "%TEMPORARY_OUTPUT_FOLDER_PATH%" /S /Q
+)
+
+IF EXIST "%NEV_BIN_FOLDER_PATH%" (
+    ECHO Deleting "%NEV_BIN_FOLDER_PATH%" folder...
+
+    RMDIR "%NEV_BIN_FOLDER_PATH%" /S /Q
+)
+IF EXIST "%NEV_OBJ_FOLDER_PATH%" (
+    ECHO Deleting "%NEV_OBJ_FOLDER_PATH%" folder...
+
+    RMDIR "%NEV_OBJ_FOLDER_PATH%" /S /Q
+)
