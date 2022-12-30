c255fe7a1242ea44bab13dd2a939e63df3d06833
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2022-12-31 00:27:29 +0700

    Refactored code (NRT).

--- src/NHibernate.ObservableCollections.Helpers/ReflectionUtil.cs
+++ src/NHibernate.ObservableCollections.Helpers/ReflectionUtil.cs
@@ -4,28 +4,28 @@ namespace NHibernate.ObservableCollections.Helpers
 {
     public static class ReflectionUtil
     {
-        private static MethodInfo _isInitializedMethod;
+        private static MethodInfo? _isInitializedMethod;
 
         public static bool IsInitialized<T>(ICollection<T> collection)
         {
-            if (_isInitializedMethod == null)
+            if (_isInitializedMethod is null)
             {
                 var type = System.Type.GetType("NHibernate.NHibernateUtil, NHibernate");
-                if (type != null)
+                if (type is not null)
                 {
                     _isInitializedMethod = type.GetMethod("IsInitialized", BindingFlags.Static | BindingFlags.Public);
                 }
             }
 
-            if (_isInitializedMethod != null)
+            if (_isInitializedMethod is not null)
             {
                 // true if the NHibernate assembly is present
                 return (bool) _isInitializedMethod.Invoke(
                     null,
-                    new object[]
+                    new object?[]
                     {
                         collection
-                    });
+                    })!;
             }
 
             return true;
@@ -34,15 +34,15 @@ namespace NHibernate.ObservableCollections.Helpers
         public static ICollection<T> NavigateToManySide<T>(object start, string propertyName)
         {
             var bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
-            var property = start.GetType().GetProperty(propertyName, bindingFlags)
-                                .DeclaringType.GetProperty(propertyName, bindingFlags);
-            return (ICollection<T>) property.GetValue(start, null);
+            var property = start.GetType().GetProperty(propertyName, bindingFlags)!
+                                .DeclaringType!.GetProperty(propertyName, bindingFlags)!;
+            return (ICollection<T>) property.GetValue(start, null)!;
         }
 
         public static object NavigateToOneSide(object start, string propertyName)
         {
-            var property = start.GetType().GetProperty(propertyName);
-            return property.GetValue(start, null);
+            var property = start.GetType().GetProperty(propertyName)!;
+            return property.GetValue(start, null)!;
         }
     }
 }
