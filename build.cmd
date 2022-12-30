00f8637d54ba69272a531f9dc2f0801d0a6192ac
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2022-12-30 21:05:32 +0700

    Updated configuration.

--- build.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
-
-@CALL build\build.cmd %*
--- build/docker-run-mono.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-MONO_TAG=${MONO_TAG:-6.12.0.182}
-docker run --rm -v "$PWD":'/project' -w='/project' mono:$MONO_TAG mono "$@"
