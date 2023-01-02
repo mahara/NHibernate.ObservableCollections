f9e962fd6b5a0589d2330979a34674c509bf6a82
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2023-01-02 14:16:04 +0700

    Refactored code.

--- src/NHibernate.ObservableCollections/ReadOnlyList.cs
+++ src/NHibernate.ObservableCollections/ReadOnlyList.cs
@@ -16,178 +16,18 @@ namespace Iesi.Collections.Generic
     /// </remarks>
     [Serializable]
     [DebuggerTypeProxy(typeof(CollectionDebugView<>))]
-    [DebuggerDisplay("Count = {Count}")]
+    [DebuggerDisplay($"{nameof(Count)} = {{{nameof(Count)}}}")]
     public class ReadOnlyList<T> :
-        IList<T>, IReadOnlyList<T>, IList
+        ReadOnlyCollection<T>
     {
-        [NonSerialized]
-        private object _syncRoot = null!;
-
-        public ReadOnlyList(IList<T> list)
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ReadOnlyList{T}" /> class
+        ///     that is a read-only wrapper around the specified list.
+        /// </summary>
+        /// <param name="list">The list to wrap.</param>
+        public ReadOnlyList(IList<T> list) :
+            base(list)
         {
-            if (list is null)
-            {
-                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.list);
-            }
-
-            InnerList = list!;
-        }
-
-        protected IList<T> InnerList { get; }
-
-        public int Count => InnerList.Count;
-
-        bool ICollection<T>.IsReadOnly => true;
-
-        bool IList.IsReadOnly => true;
-
-        public T this[int index] => InnerList[index];
-
-        T IList<T>.this[int index]
-        {
-            get => InnerList[index];
-            set => ThrowHelper.ThrowNotSupportedException(ExceptionResource.NotSupported_ReadOnlyCollection);
-        }
-
-        object? IList.this[int index]
-        {
-            get => InnerList[index];
-            set => ThrowHelper.ThrowNotSupportedException(ExceptionResource.NotSupported_ReadOnlyCollection);
-        }
-
-        object ICollection.SyncRoot
-        {
-            get
-            {
-                if (_syncRoot is null)
-                {
-                    if (InnerList is ICollection c)
-                    {
-                        _syncRoot = c.SyncRoot;
-                    }
-                    else
-                    {
-                        Interlocked.CompareExchange<object>(ref _syncRoot!, new object(), null!);
-                    }
-                }
-
-                return _syncRoot;
-            }
-        }
-
-        bool ICollection.IsSynchronized => false;
-
-        bool IList.IsFixedSize => true;
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            return InnerList.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return ((IEnumerable) InnerList).GetEnumerator();
-        }
-
-        public bool Contains(T item)
-        {
-            return InnerList.Contains(item);
-        }
-
-        bool IList.Contains(object? value)
-        {
-            if (IsCompatibleObject(value))
-            {
-                return Contains((T) value!);
-            }
-
-            return false;
-        }
-
-        public int IndexOf(T item)
-        {
-            return InnerList.IndexOf(item);
-        }
-
-        int IList.IndexOf(object? value)
-        {
-            if (IsCompatibleObject(value))
-            {
-                return IndexOf((T) value!);
-            }
-
-            return -1;
-        }
-
-        void ICollection<T>.Add(T item)
-        {
-            ThrowHelper.ThrowNotSupportedException(ExceptionResource.NotSupported_ReadOnlyCollection);
-        }
-
-        int IList.Add(object? value)
-        {
-            ThrowHelper.ThrowNotSupportedException(ExceptionResource.NotSupported_ReadOnlyCollection);
-
-            return -1;
-        }
-
-        void IList<T>.Insert(int index, T item)
-        {
-            ThrowHelper.ThrowNotSupportedException(ExceptionResource.NotSupported_ReadOnlyCollection);
-        }
-
-        void IList.Insert(int index, object? value)
-        {
-            ThrowHelper.ThrowNotSupportedException(ExceptionResource.NotSupported_ReadOnlyCollection);
-        }
-
-        bool ICollection<T>.Remove(T item)
-        {
-            ThrowHelper.ThrowNotSupportedException(ExceptionResource.NotSupported_ReadOnlyCollection);
-
-            return false;
-        }
-
-        void IList.Remove(object? value)
-        {
-            ThrowHelper.ThrowNotSupportedException(ExceptionResource.NotSupported_ReadOnlyCollection);
-        }
-
-        void IList<T>.RemoveAt(int index)
-        {
-            ThrowHelper.ThrowNotSupportedException(ExceptionResource.NotSupported_ReadOnlyCollection);
-        }
-
-        void IList.RemoveAt(int index)
-        {
-            ThrowHelper.ThrowNotSupportedException(ExceptionResource.NotSupported_ReadOnlyCollection);
-        }
-
-        void ICollection<T>.Clear()
-        {
-            ThrowHelper.ThrowNotSupportedException(ExceptionResource.NotSupported_ReadOnlyCollection);
-        }
-
-        void IList.Clear()
-        {
-            ThrowHelper.ThrowNotSupportedException(ExceptionResource.NotSupported_ReadOnlyCollection);
-        }
-
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            InnerList.CopyTo(array, arrayIndex);
-        }
-
-        void ICollection.CopyTo(Array array, int index)
-        {
-            ((ICollection) InnerList).CopyTo(array, index);
-        }
-
-        private static bool IsCompatibleObject(object? value)
-        {
-            // Non-null values are fine. Only accept nulls if T is a class or Nullable<U>.
-            // Note that default(T) is not equal to null for value types except when T is Nullable<U>.
-            return value is T || (value is null && default(T) is null);
         }
     }
 }
