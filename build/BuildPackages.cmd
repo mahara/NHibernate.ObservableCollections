6f40f6a132e104e2233756bf424e93b171831cab
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-01-12 01:18:50 +0700

    Updated configuration (part b).

--- build/BuildPackages.cmd
+++ build/BuildPackages.cmd
@@ -19,8 +19,26 @@ GOTO SET_BUILD_VERSION
 
 
 :SET_BUILD_VERSION
-IF "%version%"=="" GOTO BUILD
+IF "%version%"=="" GOTO SET_FOLDERS
 SET BUILD_VERSION=%version%
+
+GOTO SET_FOLDERS
+
+
+:SET_FOLDERS
+SET ARTIFACTS_FOLDER_NAME=artifacts
+SET ARTIFACTS_OUTPUT_FOLDER_NAME=bin
+SET ARTIFACTS_PACKAGES_FOLDER_NAME=packages
+SET ARTIFACTS_TEST_RESULTS_FOLDER_NAME=testresults
+
+SET ARTIFACTS_FOLDER_PATH=%ARTIFACTS_FOLDER_NAME%
+
+SET ARTIFACTS_OUTPUT_FOLDER_PATH=%ARTIFACTS_FOLDER_PATH%\%ARTIFACTS_OUTPUT_FOLDER_NAME%
+SET ARTIFACTS_OUTPUT_BUILD_CONFIGURATION_FOLDER_PATH=%ARTIFACTS_OUTPUT_FOLDER_PATH%\%BUILD_CONFIGURATION%
+
+SET ARTIFACTS_PACKAGES_FOLDER_PATH=%ARTIFACTS_FOLDER_PATH%\%ARTIFACTS_PACKAGES_FOLDER_NAME%\%BUILD_CONFIGURATION%
+
+SET ARTIFACTS_TEST_RESULTS_FOLDER_PATH=%ARTIFACTS_FOLDER_PATH%\%ARTIFACTS_TEST_RESULTS_FOLDER_NAME%\%BUILD_CONFIGURATION%
 
 GOTO BUILD
 
@@ -31,10 +49,12 @@ ECHO ----------------------------------------------------
 ECHO Building "%BUILD_CONFIGURATION%" packages with version "%BUILD_VERSION%"...
 ECHO ----------------------------------------------------
 
-dotnet build "NHibernate.ObservableCollections.sln" --configuration %BUILD_CONFIGURATION% --property:PACKAGE_VERSION=%BUILD_VERSION% || EXIT /B 1
+dotnet build "NHibernate.ObservableCollections-All.sln" --configuration %BUILD_CONFIGURATION% --property:PACKAGE_VERSION=%BUILD_VERSION% || EXIT /B 1
 
 dotnet build "tools\Explicit.NuGet.Versions\Explicit.NuGet.Versions.sln" --configuration Release
-"tools\Explicit.NuGet.Versions\bin\nev.exe" "bin" "NHibernate.ObservableCollections."
+SET NEV_COMMAND="%ARTIFACTS_FOLDER_PATH%\tools\nev\nev.exe" "%ARTIFACTS_PACKAGES_FOLDER_PATH%\" "NHibernate.ObservableCollections."
+ECHO %NEV_COMMAND%
+%NEV_COMMAND%
 
 GOTO TEST
 
@@ -50,22 +70,22 @@ ECHO ------------------------------------
 ECHO Running .NET (net8.0) Unit Tests
 ECHO ------------------------------------
 
-dotnet test "bin\%BUILD_CONFIGURATION%\net8.0\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.dll" --results-directory "bin\%BUILD_CONFIGURATION%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net8.0_TestResults.xml;format=nunit3"
+dotnet test "%ARTIFACTS_OUTPUT_BUILD_CONFIGURATION_FOLDER_PATH%\net8.0\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.dll" --results-directory "%ARTIFACTS_TEST_RESULTS_FOLDER_PATH%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net8.0_TestResults.xml;format=nunit3"
 
 ECHO ------------------------------------
 ECHO Running .NET (net7.0) Unit Tests
 ECHO ------------------------------------
 
-dotnet test "bin\%BUILD_CONFIGURATION%\net7.0\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.dll" --results-directory "bin\%BUILD_CONFIGURATION%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net7.0_TestResults.xml;format=nunit3"
+dotnet test "%ARTIFACTS_OUTPUT_BUILD_CONFIGURATION_FOLDER_PATH%\net7.0\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.dll" --results-directory "%ARTIFACTS_TEST_RESULTS_FOLDER_PATH%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net7.0_TestResults.xml;format=nunit3"
 
 ECHO ------------------------------------
 ECHO Running .NET (net6.0) Unit Tests
 ECHO ------------------------------------
 
-dotnet test "bin\%BUILD_CONFIGURATION%\net6.0\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.dll" --results-directory "bin\%BUILD_CONFIGURATION%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net6.0_TestResults.xml;format=nunit3"
+dotnet test "%ARTIFACTS_OUTPUT_BUILD_CONFIGURATION_FOLDER_PATH%\net6.0\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.dll" --results-directory "%ARTIFACTS_TEST_RESULTS_FOLDER_PATH%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net6.0_TestResults.xml;format=nunit3"
 
 ECHO --------------------------------------------
 ECHO Running .NET Framework (net48) Unit Tests
 ECHO --------------------------------------------
 
-dotnet test "bin\%BUILD_CONFIGURATION%\net48\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.exe" --results-directory "bin\%BUILD_CONFIGURATION%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net48_TestResults.xml;format=nunit3"
+dotnet test "%ARTIFACTS_OUTPUT_BUILD_CONFIGURATION_FOLDER_PATH%\net48\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.dll" --results-directory "%ARTIFACTS_TEST_RESULTS_FOLDER_PATH%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net48_TestResults.xml;format=nunit3"
