2c7f6d9bbff25ae9e7506317910cdf4f20be91a1
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-01-12 01:18:50 +0700

    Updated configuration (part b).

--- CleanPackages.cmd
+++ CleanPackages.cmd
@@ -1,32 +1,15 @@
 @ECHO OFF
 
 
-SET OUTPUT_FOLDER_PATH=bin
-SET TEMPORARY_OUTPUT_FOLDER_PATH=obj
-SET NEV_BIN_FOLDER_PATH=tools\Explicit.NuGet.Versions\bin
-SET NEV_OBJ_FOLDER_PATH=tools\Explicit.NuGet.Versions\obj
+SET ARTIFACTS_FOLDER_NAME=artifacts
+
+SET ARTIFACTS_FOLDER_PATH=%ARTIFACTS_FOLDER_NAME%
 
 REM dotnet clean "%1" --configuration Debug
 REM dotnet clean "%1" --configuration Release
 
-IF EXIST "%OUTPUT_FOLDER_PATH%" (
-    ECHO Deleting "%OUTPUT_FOLDER_PATH%" folder...
+IF EXIST "%ARTIFACTS_FOLDER_PATH%" (
+    ECHO Deleting "%ARTIFACTS_FOLDER_PATH%" folder...
 
-    RMDIR "%OUTPUT_FOLDER_PATH%" /S /Q
-)
-IF EXIST "%TEMPORARY_OUTPUT_FOLDER_PATH%" (
-    ECHO Deleting "%TEMPORARY_OUTPUT_FOLDER_PATH%" folder...
-
-    RMDIR "%TEMPORARY_OUTPUT_FOLDER_PATH%" /S /Q
-)
-
-IF EXIST "%NEV_BIN_FOLDER_PATH%" (
-    ECHO Deleting "%NEV_BIN_FOLDER_PATH%" folder...
-
-    RMDIR "%NEV_BIN_FOLDER_PATH%" /S /Q
-)
-IF EXIST "%NEV_OBJ_FOLDER_PATH%" (
-    ECHO Deleting "%NEV_OBJ_FOLDER_PATH%" folder...
-
-    RMDIR "%NEV_OBJ_FOLDER_PATH%" /S /Q
+    RMDIR "%ARTIFACTS_FOLDER_PATH%\" /S /Q
 )
