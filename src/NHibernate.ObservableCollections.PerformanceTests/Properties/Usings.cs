438bec52d0da434c9dd4d5478589a872ff95ae72
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-06-28 16:51:39 +0700

    Added PerformanceTests project.

---
+++ src/NHibernate.ObservableCollections.PerformanceTests/Properties/Usings.cs
@@ -0,0 +1,9 @@
+global using BenchmarkDotNet.Attributes;
+global using BenchmarkDotNet.Configs;
+global using BenchmarkDotNet.Diagnosers;
+global using BenchmarkDotNet.Environments;
+global using BenchmarkDotNet.Exporters;
+global using BenchmarkDotNet.Jobs;
+global using BenchmarkDotNet.Running;
+
+global using NHibernate.ObservableCollections.PerformanceTests;
