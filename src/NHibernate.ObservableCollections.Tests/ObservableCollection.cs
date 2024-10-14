d4a86e8a8ceb1020a1d133cc6e30f4546b96c358
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-10-14 19:40:20 +0700

    Refactored code (part 001).

---
+++ src/NHibernate.ObservableCollections.Tests/ObservableCollection.cs
@@ -0,0 +1,85 @@
+using System.ComponentModel;
+
+namespace Iesi.Collections.Generic.Tests
+{
+    public class TestObservableCollection<T> : ObservableCollection<T>
+    {
+        public TestObservableCollection()
+        {
+        }
+
+        public TestObservableCollection(IEnumerable<T> collection) :
+            base(collection)
+        {
+        }
+
+        public TestObservableCollection(List<T> collection) :
+            base(collection)
+        {
+        }
+
+        public List<NotifyCollectionChangedEventArgs> CollectionChangedEventArgsList { get; } = [];
+
+        public List<PropertyChangedEventArgs> PropertyChangedEventArgsList { get; } = [];
+
+        public void ResetAllEventArgsLists()
+        {
+            CollectionChangedEventArgsList.Clear();
+
+            PropertyChangedEventArgsList.Clear();
+        }
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnCollectionChanged(e);
+
+            if (!EventNotificationsAreDeferred)
+            {
+                CollectionChangedEventArgsList.Add(e);
+            }
+        }
+
+        protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+
+            if (!EventNotificationsAreDeferred)
+            {
+                PropertyChangedEventArgsList.Add(e);
+            }
+        }
+    }
+
+    public class TestReadOnlyObservableCollection<T> : ReadOnlyObservableCollection<T>
+    {
+        public TestReadOnlyObservableCollection(ObservableCollection<T> collection) :
+            base(collection)
+        {
+        }
+
+        public List<NotifyCollectionChangedEventArgs> CollectionChangedEventArgsList { get; } = [];
+
+        public List<PropertyChangedEventArgs> PropertyChangedEventArgsList { get; } = [];
+
+        public void ResetAllEventArgsLists()
+        {
+            CollectionChangedEventArgsList.Clear();
+
+            PropertyChangedEventArgsList.Clear();
+        }
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnCollectionChanged(e);
+
+            CollectionChangedEventArgsList.Add(e);
+        }
+
+        protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+
+            PropertyChangedEventArgsList.Add(e);
+        }
+    }
+}
