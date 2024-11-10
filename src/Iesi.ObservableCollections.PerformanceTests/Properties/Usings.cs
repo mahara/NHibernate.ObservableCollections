fcdace18d6262cfb1c05ef4b874d8e75b22321c6
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-11-10 16:51:39 +0700

    Added PerformanceTests project.

---
+++ src/Iesi.ObservableCollections.PerformanceTests/Properties/Usings.cs
@@ -0,0 +1,7 @@
+global using BenchmarkDotNet.Attributes;
+global using BenchmarkDotNet.Configs;
+global using BenchmarkDotNet.Diagnosers;
+global using BenchmarkDotNet.Exporters;
+global using BenchmarkDotNet.Jobs;
+
+global using Iesi.Collections.Generic;
