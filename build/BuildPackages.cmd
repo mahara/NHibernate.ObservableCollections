d628ab7e7b6a80da5833fa633c3285644db7f633
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-12-08 13:25:18 +0700

    Upgraded to .NET 9.0.

--- build/BuildPackages.cmd
+++ build/BuildPackages.cmd
@@ -90,7 +90,7 @@ FOR %%G IN (%BUILD_PARAMETERS%) DO (
 
 SET ARTIFACTS_PACKAGES___CONFIGURATION___FOLDER_PATH=%ARTIFACTS_PACKAGES_FOLDER_PATH%\%BUILD_PARAMETER___CONFIGURATION%
 
-dotnet build "%WORKSPACE_FOLDER_PATH%\tools\Explicit.NuGet.Versions\Explicit.NuGet.Versions.sln" --configuration Release
+dotnet build "%WORKSPACE_FOLDER_PATH%\tools\Explicit.NuGet.Versions\Explicit.NuGet.Versions.slnx" --configuration Release
 
 FOR %%G IN (%PACKAGE_PARAMETERS%) DO (
     SET PACKAGE_PARAMETER___PACKAGE_ID_PREFIX_FILTER=%%G
