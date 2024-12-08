5ed842248558c104c1a3024b36906e579c767c6a
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-12-08 13:25:18 +0700

    Upgraded to .NET 9.0.

--- build/BuildPackages.cmd
+++ build/BuildPackages.cmd
@@ -1,30 +1,37 @@
 @ECHO OFF
 
+REM
+REM https://stackoverflow.com/questions/15420004/write-batch-file-with-hyphenated-parameters
+REM https://superuser.com/questions/1505178/parsing-command-line-argument-in-batch-file
+REM
 
-SET %1
-SET %2
 
 SET BUILD_CONFIGURATION=Release
 SET BUILD_VERSION=1.0.0
-
-GOTO SET_BUILD_CONFIGURATION
-
-
-:SET_BUILD_CONFIGURATION
-IF "%configuration%"=="" GOTO SET_BUILD_VERSION
-SET BUILD_CONFIGURATION=%configuration%
-
-GOTO SET_BUILD_VERSION
+SET NO_TEST=
 
 
-:SET_BUILD_VERSION
-IF "%version%"=="" GOTO SET_FOLDERS
-SET BUILD_VERSION=%version%
+:PARSE_ARGS
 
-GOTO SET_FOLDERS
+IF {%1} == {} GOTO SET_FOLDERS
+
+IF /I "%1" == "--configuration" (
+    SET "BUILD_CONFIGURATION=%2" & SHIFT
+)
+IF /I "%1" == "--version" (
+    SET "BUILD_VERSION=%2" & SHIFT
+)
+IF /I "%1" == "--no-test" (
+    SET NO_TEST=true
+)
+
+SHIFT
+
+GOTO PARSE_ARGS
 
 
 :SET_FOLDERS
+
 SET ARTIFACTS_FOLDER_NAME=artifacts
 SET ARTIFACTS_OUTPUT_FOLDER_NAME=bin
 SET ARTIFACTS_PACKAGES_FOLDER_NAME=packages
@@ -55,7 +62,7 @@ SET NEV_COMMAND="%ARTIFACTS_FOLDER_PATH%\tools\nev\nev.exe" "%ARTIFACTS_PACKAGES
 ECHO %NEV_COMMAND%
 %NEV_COMMAND%
 
-GOTO TEST
+IF DEFINED NO_TEST GOTO EXIT
 
 
 :TEST
@@ -66,25 +73,22 @@ REM https://github.com/Microsoft/vstest-docs/blob/main/docs/report.md
 REM https://github.com/spekt/nunit.testlogger/issues/56
 
 ECHO ------------------------------------
+ECHO Running .NET (net9.0) Unit Tests
+ECHO ------------------------------------
+
+dotnet test "%ARTIFACTS_OUTPUT_BUILD_CONFIGURATION_FOLDER_PATH%\net9.0\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.dll" --results-directory "%ARTIFACTS_TEST_RESULTS_FOLDER_PATH%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net9.0_TestResults.xml;format=nunit3"
+
+ECHO ------------------------------------
 ECHO Running .NET (net8.0) Unit Tests
 ECHO ------------------------------------
 
 dotnet test "%ARTIFACTS_OUTPUT_BUILD_CONFIGURATION_FOLDER_PATH%\net8.0\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.dll" --results-directory "%ARTIFACTS_TEST_RESULTS_FOLDER_PATH%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net8.0_TestResults.xml;format=nunit3"
-
-ECHO ------------------------------------
-ECHO Running .NET (net7.0) Unit Tests
-ECHO ------------------------------------
-
-dotnet test "%ARTIFACTS_OUTPUT_BUILD_CONFIGURATION_FOLDER_PATH%\net7.0\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.dll" --results-directory "%ARTIFACTS_TEST_RESULTS_FOLDER_PATH%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net7.0_TestResults.xml;format=nunit3"
-
-ECHO ------------------------------------
-ECHO Running .NET (net6.0) Unit Tests
-ECHO ------------------------------------
-
-dotnet test "%ARTIFACTS_OUTPUT_BUILD_CONFIGURATION_FOLDER_PATH%\net6.0\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.dll" --results-directory "%ARTIFACTS_TEST_RESULTS_FOLDER_PATH%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net6.0_TestResults.xml;format=nunit3"
 
 ECHO --------------------------------------------
 ECHO Running .NET Framework (net48) Unit Tests
 ECHO --------------------------------------------
 
 dotnet test "%ARTIFACTS_OUTPUT_BUILD_CONFIGURATION_FOLDER_PATH%\net48\NHibernate.ObservableCollections.Tests\NHibernate.ObservableCollections.Tests.exe" --results-directory "%ARTIFACTS_TEST_RESULTS_FOLDER_PATH%" --logger "nunit;LogFileName=NHibernate.ObservableCollections_net48_TestResults.xml;format=nunit3"
+
+
+:EXIT
