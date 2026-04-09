afc7f703563f674c51f3f698dc64a1d4b5797643
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2026-04-10 00:00:00 +0700

    Refactored code (PerformanceTests).

--- src/Iesi.ObservableCollections.PerformanceTests/Program.cs
+++ src/Iesi.ObservableCollections.PerformanceTests/Program.cs
@@ -1,31 +1,26 @@
-using BenchmarkDotNet.Environments;
-
-internal class Program
+internal static class Program
 {
     private static void Main(string[] args)
     {
-        var baseJob = Job.Default;
-
-        var jobNet100 = baseJob.WithRuntime(CoreRuntime.Core10_0).WithBaseline(true);
-        var jobNet90 = baseJob.WithRuntime(CoreRuntime.Core90);
-        var jobNet80 = baseJob.WithRuntime(CoreRuntime.Core80);
-        var jobNet48 = baseJob.WithRuntime(ClrRuntime.Net48);
-
         var baseConfig = DefaultConfig.Instance;
 
-        var config = baseConfig
-            .AddJob(jobNet100)
-            .AddJob(jobNet90)
-            .AddJob(jobNet80)
-            .AddJob(jobNet48)
-            .AddDiagnoser(MemoryDiagnoser.Default)
-            .AddExporter(RPlotExporter.Default);
+        //var baseConfig = ManualConfig.CreateEmpty()
+        //                             .AddLogger(BenchmarkDotNet.Loggers.ConsoleLogger.Default)
+        //                             .AddExporter(BenchmarkDotNet.Exporters.MarkdownExporter.Default)
+        //                             .AddColumnProvider(BenchmarkDotNet.Columns.DefaultColumnProviders.Instance);
 
 
 
-        //BenchmarkRunner.Run<LoopsBenchmarks>(config, args);
+        var baseJob = Job.Default;
 
-        //BenchmarkRunner.Run<ObservableCollection_Benchmarks>(config, args);
-        //BenchmarkRunner.Run<ObservableCollection_ReplaceRange_Benchmarks>(config, args);
+
+
+        var baseFeatures = BenchmarkFeatures.None;
+
+
+
+        var type = typeof(Program);
+
+        BenchmarkExecutor.Execute(args, type, baseConfig, baseJob, baseFeatures);
     }
 }
