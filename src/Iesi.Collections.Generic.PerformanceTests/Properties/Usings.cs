fb974968345460dbfa2139ce68c34ea14eddb226
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2026-04-10 00:00:00 +0700

    Refactored code (PerformanceTests).

--- src/Iesi.Collections.Generic.PerformanceTests/Properties/Usings.cs
+++ src/Iesi.Collections.Generic.PerformanceTests/Properties/Usings.cs
@@ -5,3 +5,5 @@ global using BenchmarkDotNet.Environments;
 global using BenchmarkDotNet.Exporters;
 global using BenchmarkDotNet.Jobs;
 global using BenchmarkDotNet.Running;
+
+global using Iesi.Collections.Generic.PerformanceTests;
